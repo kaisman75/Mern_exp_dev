a7d176975ef55639a1f596b685ddcc9d031d26e8
kais manai <kaismanai1975@gmail.com>
2021-09-21 10:47:13 +0100

    first step

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-const app= require('./src/app')
-require('dotenv').config()
+const express= require("express");
+const app=express()
+require('dotenv').config();
+
+app.use('/',(req,res)=>{
+    res.send("hello kais")
+})
+
 const PORT=process.env.PORT||5000;
 
 app.listen(PORT,()=>{
